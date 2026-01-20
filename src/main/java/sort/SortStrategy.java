2505442db7fa04ba33e7846713c4c2fbcb780ef6
Vladimir Partko <partko.vl.3@gmail.com>
2026-01-20 21:13:19 +0300

    Update project structure

--- src/main/java/sort/SortStrategy.java
+++ src/main/java/sort/SortStrategy.java
@@ -1,4 +1,10 @@
 package sort;
 
-public interface SortStrategy {
+import collection.CustomList;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public interface SortStrategy<T> {
+    void sort(CustomList<T> data, Comparator<T> comparator);
 }
